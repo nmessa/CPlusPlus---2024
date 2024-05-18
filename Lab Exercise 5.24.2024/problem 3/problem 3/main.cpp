f958513d64f5329672aa236d120df7b879a45ab1
nmessa <nmessa@sau16.org>
2024-05-17 20:33:35 -0400

    Add files via upload

---
+++ Lab Exercise 5.24.2024/problem 3/problem 3/main.cpp
@@ -0,0 +1,20 @@
+//Lab Exercise 5.24.2024 Problem 3
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Add prototype here
+
+int main()
+{
+	cout << functionThree("Python") << endl;
+	cout << functionThree("ytade") << endl;
+	cout << functionThree("jsues") << endl;
+
+	return 0;
+}
+
+//Add function here
+
